601305f6b9c9b63b07e58a76bae34ebaf34a8678
David <sparkster187@gmail.com>
2019-05-06 05:51:20 +0100

    Level Update

--- Assets/Scripts/Enemy/Enemy_Health.cs
+++ Assets/Scripts/Enemy/Enemy_Health.cs
@@ -15,18 +15,19 @@ public class Enemy_Health : MonoBehaviour {
     public bool isDead;
     //public int R_dropRate;
 
-    public int EXPGained;
+    float timer = 3.9f;
+    public int EXPToGain;
     private Player_Level Player_stats;
 
 	// Use this for initialization
 	void Start ()
   {
-        currentHealth = fullHealth;
+        currentHealth = fullHealth;//enemy starts off with full health 
         enemy = this.gameObject;
         anim = enemy.GetComponent<Animator>();
         anim.SetBool("IsDead", false);
         isDead = false;
-
+       
         Player_stats = FindObjectOfType<Player_Level>();
 
   }
@@ -34,7 +35,25 @@ public class Enemy_Health : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
   {
-    if (enemy != null)
+        if (currentHealth <= 0)
+        {
+            if (timer >= 0)
+            {
+                timer -= Time.deltaTime;
+                enemy.GetComponent<AI>().enabled = false;//this stops the enemy death animation from constantly running and keeps the already defeated enemies from following the player
+
+            }
+            else
+            {
+                // Add your death things here...
+                Debug.Log("runs");
+                Destroyenemy();
+                Destroy(this.gameObject);
+                Player_stats.AddEXP(EXPToGain);
+
+            }
+        }
+        if (enemy != null)
     {
       EnemyHealth.transform.position = enemy.transform.position + enemy.transform.up + new Vector3(0f, 0.2f, 0f);
     }
@@ -50,11 +69,16 @@ public class Enemy_Health : MonoBehaviour {
     if(currentHealth <= 0)
     {
       anim.SetBool("IsDead", true);
-      Destroyenemy();
-
-      Player_stats.AddEXP(EXPGained);
+            
+            //if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Death"))
+            //{
+            //    Debug.Log("runs");
+            //    Destroyenemy();
+            //    Destroy(this.gameObject);
+            //    Player_stats.AddEXP(EXPToGain);
+            //}
+        }
     }
-  }
 
    
 
--- Assets/Scripts/Player/Player_Health.cs
+++ Assets/Scripts/Player/Player_Health.cs
@@ -12,10 +12,6 @@ public class Player_Health : MonoBehaviour {
   public Image damagePlayer;
   public float flashSpeed = 5f;
   public Color flashColour = new Color(1f, 0, 0, 1f);
-
-  public int Player_level;
-  public int EXP;
-  public double Level_Limit;
     
     Animator anim;
 
@@ -26,81 +22,66 @@ public class Player_Health : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
   {
-    currentHealth = initialHealth;
-    player = GameObject.FindGameObjectWithTag("Player");
-    anim = player.gameObject.GetComponent<Animator>();
-
-        Player_level = 1;
-        EXP = 0;
-        Level_Limit = 100;
+    currentHealth = initialHealth;//Current health is equal to the maximum health at the start of the game
+    player = GameObject.FindGameObjectWithTag("Player");//sets the player to be the object with the tag player
+    anim = player.gameObject.GetComponent<Animator>();//variable that stores component from the animator 
         
   }
 	
 	// Update is called once per frame
 	void Update ()
   {
-        if (EXP >= Level_Limit)
-        {
-            Player_level += 1;
-            EXP = 0;
-            Level_Limit *= 1.5;
-            initialHealth += 10;
-        }
-
-    if (damaged && currentHealth <= 10)
+    if (damaged && currentHealth <= 10)//checks if the player's health is less than or equal to 10
     {
-      damagePlayer.color = flashColour;
+      damagePlayer.color = flashColour;//flashes the set colour on the screen
     }
     else
     {
       damagePlayer.color = Color.Lerp(damagePlayer.color, Color.clear, flashSpeed * Time.deltaTime);
     }
-    damaged = false;
+    damaged = false;//states that the player has not been damaged
 	}
 
  public void AddDamage(float damage)
   {
-    damaged = true;
+    damaged = true;//states that the enemy has been damaged 
 
-    currentHealth -= damage;
-    healthSlider.value = currentHealth;
+    currentHealth -= damage;//lowers player's health by the amount of damage dealt by the enemy
+    healthSlider.value = currentHealth;//moves the health slider so that the player knows their current health situation and has an idea as to how much damage they have taken
 
-    if(currentHealth <= 0 && isDead == false)
+    if(currentHealth <= 0 && isDead == false)//checks if player's health is equal to or less than 0 and if isDead is equal to false
     {
-      Destroyplayer();
+      Destroyplayer();//goes to the destroy player function
     }
   }
 
   public void Destroyplayer()
   {
     
-    isDead = true;
+    isDead = true;//sets the state of isDead to true
     if (player != null)
     {
-      Destroy(gameObject);
-      player = null;
+      Destroy(gameObject);//destroys the player object
+      player = null;//makes the player object null so that they have no value and cannot be used while in this state
     }
   }
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Ramen")
+        if (other.gameObject.name == "Ramen")//checks if the player collides with an object with the stated name
         {
-            Destroy(other.gameObject);
+            Destroy(other.gameObject);//destroys the other object
             Debug.Log("We hit ramen");
-            currentHealth += 50;
+            currentHealth += 50;//increases player's current health by 50
             if (currentHealth > initialHealth)
             {
-                currentHealth = initialHealth;
+                currentHealth = initialHealth;//stops the players current health from exceeding their max health
             }
-            healthSlider.value = currentHealth;
+            healthSlider.value = currentHealth;//moves the health slider UI bar to show how much health is restored
 
         }
-    }
 
-    public void OnCollisionEnter(Collider other)
-    {
-        if (other.gameObject.name == "BentoBox")
+        else if(other.gameObject.name == "BentoBox")
         {
             Destroy(other.gameObject);
             Debug.Log("We hit the Bento box");
@@ -110,7 +91,13 @@ public class Player_Health : MonoBehaviour {
                 currentHealth = initialHealth;
             }
             healthSlider.value = currentHealth;
-
         }
+    }
+
+    public void Increase_health(int HP_Up)
+    {
+        initialHealth += HP_Up;//Adds the value to the max health when the player levels up
+        currentHealth += 10;
+        healthSlider.value = currentHealth;
     }
 }
